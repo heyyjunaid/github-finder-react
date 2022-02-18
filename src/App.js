ca2fc8c8a8913a6aac0e7519eea2605071d9786a
Junaid Qureshi <Junaid.562000@gmail.com>
2022-02-18 15:31:52 +0530

    pages done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 function App() {
   return (
-    <div>
+    <>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container px-3 mx-auto pb-12">content</main>
+        <main className="container px-3 mx-auto pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
-    </div>
+    </>
   );
 }
 
