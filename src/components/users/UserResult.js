3d9e8fe40c2b56bfc2e02fbef8bb1ac3a3dc1c4c
Junaid Qureshi <Junaid.562000@gmail.com>
2022-02-19 08:44:10 +0530

    context added

--- src/components/users/UserResult.js
+++ src/components/users/UserResult.js
@@ -2,24 +2,14 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
+import { useContext } from 'react';
+import GithubContext from '../../context/github/GithubContext';
 const UserResult = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
+  const { users, loading, fetchUsers } = useContext(GithubContext);
   useEffect(() => {
     fetchUsers();
   }, []);
-  const fetchUsers = async () => {
-    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-    const data = await res.json();
-    console.log(data);
-    setUsers(data);
-    setLoading(false);
-  };
+
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
